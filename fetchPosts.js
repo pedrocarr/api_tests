2898087d539e194eca4c9a3fa960bec60d32160d
pedrocarr <eng.pedro.amb@gmail.com>
2025-05-25 19:27:14 +0200

    warmup challenge complete

--- fetchPosts.js
+++ fetchPosts.js
@@ -6,12 +6,21 @@ async function fetchPosts() {
   try {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
 
+
+    if (response.status !== 200) {
+      throw new Error(`Response Status is ${response.status}`)
+    }
+
     for (const post of response.data) {
-      results.push(post)
+      if (post.id === 1) {
+        results.push(post)
+
+      }
     }
     writeFileSync('posts.json', JSON.stringify(results, null, 2), 'utf-8')
+    return results
   } catch (error) {
-    throw new Error('Error fetching the posts', error)
+    console.error('Error fetching the posts', error)
   }
 }
 
--- warmup-exercise.js
+++ warmup-exercise.js
@@ -3,3 +3,19 @@
  * 2. Make a request to: https://www.craiglist.org/about/
  * 3. Compare what you see in Node to vistiting the actual page in your Browser
  */
+
+
+async function fetchHtml() {
+  try {
+    const response = await fetch('https://www.craigslist.org/ab/')
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.text()
+    console.log(data)
+  } catch (error) {
+    console.error('Error fetching the HTML', error)
+  }
+}
+
+fetchHtml()
