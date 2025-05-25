3b9c5dcf20b53632a3fd088fea0c6e2557171564
pedrocarr <eng.pedro.amb@gmail.com>
2025-05-25 19:45:02 +0200

    exercise1 complete

---
+++ exercise1.js
@@ -0,0 +1,27 @@
+import { writeFileSync } from 'fs'
+import  axios from 'axios'
+
+async function fetchPosts() {
+  const results = []
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+
+    if (response.status !== 200) {
+      throw new Error(`Response Status is ${response.status}`)
+    }
+
+    for (const post of response.data) {
+      if (post.id === 1) {
+        results.push(post)
+
+      }
+    }
+    writeFileSync('posts.json', JSON.stringify(results, null, 2), 'utf-8')
+    return results
+  } catch (error) {
+    console.error('Error fetching the posts', error)
+  }
+}
+
+fetchPosts()
